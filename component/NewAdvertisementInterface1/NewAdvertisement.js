9bc3f04971284a9cb3516ea7d550b0b3817a3744
IT19006994 - K.H.K.L. De Silva <87925569+IT19006994@users.noreply.github.com>
2021-09-28 19:01:35 +0530

    Updated

--- component/NewAdvertisementInterface1/NewAdvertisement.js
+++ component/NewAdvertisementInterface1/NewAdvertisement.js
@@ -5,6 +5,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default function NewAdvertisement({navigation}){
+    //Next Page
     const nextPage = () =>{
         navigation.push('NewAdvertisement2');
     };
